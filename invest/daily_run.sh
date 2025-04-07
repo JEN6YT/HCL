1fee22bd0665bca27fe89ed02c72d7fe05523bbb
JEN6YT <71663327+JEN6YT@users.noreply.github.com>
2025-04-07 18:54:30 -0400

    delete <> folder

--- invest/daily_run.sh
+++
@@ -1,4 +0,0 @@
-
-python3 update_prod.py > /home/ubuntu/code/HCL/invest/data/prod/latest_update_log 
-#python3 trade.py > /home/ubuntu/code/HCL/invest/data/prod/latest_trade_log 
-pkill gunicorn; gunicorn -b 0.0.0.0:8000 -w 2 app:app
