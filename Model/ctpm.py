d1db7f83b62c0a2a34305ff9e9aa61285b1f187e
JEN6YT <71663327+JEN6YT@users.noreply.github.com>
2025-06-06 18:33:20 -0400

    Update ctpm.py

--- Model/ctpm.py
+++ Model/ctpm.py
@@ -1,5 +1,6 @@
 # Continuous Treatment Propensity Model
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,7 +10,7 @@ def soft_abs(x, beta=40.0):
     return torch.log(1 + torch.exp(beta * x)) + torch.log(1 + torch.exp(-beta * x)) / beta
 
 class CTPM(nn.Module):
-    def __init__(self, D_dim, num_hidden, temp, p_quantile, dropout_rate):
+    def __init__(self, Da_dim, Db_dim, num_hidden, temp, p_quantile, dropout_rate):
         """
         D_dim: feature dimension
         num_hidden: number of hidden units in the network
@@ -21,34 +22,57 @@ class CTPM(nn.Module):
         self.p_quantile = p_quantile
         self.dropout_rate = dropout_rate
 
+        # Prior network
+        if num_hidden > 0:
+            self.hidden_prior = nn.Sequential(
+                nn.Linear(Da_dim, num_hidden),
+                nn.Tanh(),
+                nn.Dropout(dropout_rate)
+            )
+            self.prior_score = nn.Linear(num_hidden, 1)
+        else:
+            self.prior_score = nn.Linear(Da_dim, 1)
+
         # Match network
         if num_hidden > 0:
             self.hidden_match = nn.Sequential(
-                nn.Linear(D_dim, num_hidden),
+                nn.Linear(Da_dim+Db_dim, num_hidden),
                 nn.Tanh(),
                 nn.Dropout(dropout_rate)
             )
             self.match_score = nn.Linear(num_hidden, 1)
         else:
-            self.match_score = nn.Linear(D_dim, 1)
+            self.match_score = nn.Linear(Da_dim+Db_dim, 1)
 
         # Policy network on Da+Db
         if num_hidden > 0:
             self.hidden_policy = nn.Sequential(
-                nn.Linear(D_dim, num_hidden),
+                nn.Linear(Da_dim+Db_dim, num_hidden),
                 nn.Tanh(),
                 nn.Dropout(dropout_rate)
             )
             self.policy_score = nn.Linear(num_hidden, 1)
         else:
-            self.policy_score = nn.Linear(D_dim, 1)
+            self.policy_score = nn.Linear(Da_dim+Db_dim, 1)
 
-    def forward(self, D_tre, D_unt, o_tre, o_unt, c_tre, c_unt, i_tre, i_unt):
+        # Cosine similarity embeddings
+        self.match_embed_a = nn.Sequential(
+            nn.Linear(Da_dim, num_hidden),
+            nn.Tanh()
+        )
+        self.match_embed_b = nn.Sequential(
+            nn.Linear(Db_dim, num_hidden),
+            nn.Tanh()
+        )
+
+    def forward(self, Da_tre, Da_unt, Db_tre, Db_unt, o_tre, o_unt, c_tre, c_unt, i_tre, i_unt):
                 # d2d_tre, d2d_unt, d2dlamb):
 
         """
-        D_tre: treatment data
-        D_unt: control data
+        Da_tre: treatment data, user features
+        Da_unt: control data
+        Db_tre: treatment data, coupon features
+        Db_unt: control data
         o_tre: treatment reward
         o_unt: control reward
         c_tre: treatment cost
@@ -56,6 +80,19 @@ class CTPM(nn.Module):
         i_tre: treatment intensity (ex. number of working hours)
         i_unt: control intensity (ex. number of working hours)
         """
+
+        D_tre = torch.cat((Da_tre, Db_tre), axis = 1) 
+        D_unt = torch.cat((Da_unt, Db_unt), axis = 1) 
+
+        # Prior scores
+        if self.num_hidden > 0:
+            tre_prior_hidden = self.hidden_prior(Da_tre)
+            unt_prior_hidden = self.hidden_prior(Da_unt)
+            p_tre = torch.sigmoid(self.prior_score(tre_prior_hidden))
+            p_unt = torch.sigmoid(self.prior_score(unt_prior_hidden))
+        else:
+            p_tre = torch.sigmoid(self.prior_score(tre_prior_hidden))
+            p_unt = torch.sigmoid(self.prior_score(unt_prior_hidden))
 
         # Match scores
         if self.num_hidden > 0:
@@ -77,12 +114,34 @@ class CTPM(nn.Module):
             tre_policy_score = torch.sigmoid(self.policy_score(D_tre))
             unt_policy_score = torch.sigmoid(self.policy_score(D_unt))
 
+        # Compute cosine similarity embeddings
+        tre_embed_a = self.match_embed_a(Da_tre)
+        tre_embed_b = self.match_embed_b(Db_tre)
+        unt_embed_a = self.match_embed_a(Da_unt)
+        unt_embed_b = self.match_embed_b(Db_unt)
+        # Cosine similarity + 1
+        cos_sim_tre = F.cosine_similarity(tre_embed_a, tre_embed_b, dim=1) + 1
+        cos_sim_unt = F.cosine_similarity(unt_embed_a, unt_embed_b, dim=1) + 1
+
+        if self.eval():
+            cos_sim_tre = torch.ones_like(cos_sim_tre)
+            cos_sim_unt = torch.ones_like(cos_sim_unt)
+
+        s_tre = (p_tre * s_tre * cos_sim_tre).squeeze()
+        s_unt = (p_unt * s_unt * cos_sim_unt).squeeze()
+        # normalize scores
+        s_tre = s_tre / (s_tre.sum() + 1e-17)
+        s_unt = s_unt / (s_unt.sum() + 1e-17)
 
         # Bell-shaped intensity score
 
         # difference between actually treatment intensity and predicted optimal intensity
         diff_tre = i_tre.squeeze() - tre_policy_score.squeeze()
         diff_unt = i_unt.squeeze() - unt_policy_score.squeeze()
+
+        if self.eval():
+            diff_tre = torch.zero_like(diff_tre)
+            diff_unt = torch.zero_like(diff_unt)
         # derivative of sigmoid has a bell-shape curve
         # it peaks at diff = 0
         # decreases symmetrically as diff moves away from 0
